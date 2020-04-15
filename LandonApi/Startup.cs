239f49dadd0e2147847fee497ae2f3f238399a36
Mitesh Shah <mitsha@microsoft.com>
2020-04-15 14:39:50 +0530

    02_02 Configuring MVC

--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace LandonApi
 {
@@ -26,6 +27,10 @@ namespace LandonApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
